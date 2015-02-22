290e9ba0d8a8b6034f0a33ac005fc20f033fade5
k-stachowiak <krzysiek.stachowiak@gmail.com>
2015-02-22 13:54:14 +0100

    Added copmare argument to the basic optimization algorithms.

--- algorithms_basic.h
+++ algorithms_basic.h
@@ -23,22 +23,6 @@ bool all_equal(const T& x, const T& y, const Tail&... tail) {
 	return (x == y) && all_equal(y, tail...);
 }
 
-// Topological structure alanysis algorithms.
-// ==========================================
-
-template <class Metric, class Topology>
-typename Metric::weight_type metric_add(const Topology& t, const Metric& m)
-{
-    using Weight = typename Metric::weight_type;
-    Weight result = weight_limits<Weight>::zero;
-
-    for_each_edge(t, [&result, &m](const edge& e) {
-        result += m(e);
-    });
-
-    return result;
-}
-
 // Topological structure building algorithms.
 // ==========================================
 
@@ -89,22 +73,28 @@ namespace detail {
     /// @tparam Graph A Topology,
     /// @tparam Metric A Metric,
     /// @tparam Stop A functor defining the stop condition for the algorithm.
+    /// @tparam WeightCmp A functor providing the means of comparing weights.
     ///
     /// @param g The graph,
     /// @param m The metric,
-    /// @param stop The stop condition functor,
     /// @param src The source for the relaxation,
     /// @param out_preds The out parameter returning the predecessors' map,
     /// @param out_dists The out parameter returning the distances' map.
+    /// @param stop The stop condition functor,
+    /// @param cmp The weight comparator functor,
     ///
-	template <class Graph, class Metric, typename Stop>
+	template <class Graph, class Metric, typename Stop, typename WeightCmp>
 	void dijkstra_relax(
-			const Graph& g, const Metric& m,
-			Stop stop, node src,
+			const Graph& g,
+            const Metric& m,
+            node src,
 			std::vector<node>& out_preds,
-			std::vector<typename Metric::weight_type>& out_dists) {
+			std::vector<typename Metric::weight_type>& out_dists,
+			Stop stop,
+            const WeightCmp& cmp) {
 
-		node N = static_cast<node>(nodes_count(g));
+		const node N = static_cast<node>(nodes_count(g));
+
 		std::set<node> open;
 		open.insert(src);
 
@@ -130,9 +120,9 @@ namespace detail {
             std::for_each(
                 g.out_begin(u),
                 g.out_end(u),
-                [u, &out_dists, &out_preds, &m, &open](node v) mutable {
+                [u, &out_dists, &out_preds, &m, &open, &cmp](node v) mutable {
                     typename Metric::weight_type new_dist = out_dists[u] + m(edge(u, v));
-                    if (new_dist < out_dists[v]) {
+                    if (cmp(new_dist, out_dists[v])) {
                         out_dists[v] = new_dist;
                         out_preds[v] = u;
                         open.insert(v);
@@ -147,18 +137,22 @@ namespace detail {
     ///
     /// @tparam Graph A Topology,
     /// @tparam Metric A Metric,
+    /// @tparam WeightCmp A functor providing the means of comparing weights.
     ///
     /// @param g The graph,
     /// @param m The metric,
     /// @param src The source of the relaxation,
     /// @param out_preds The out parameter returning the predecessors' map,
     /// @param out_dists The out parameter returning the distances' map.
-	template <class Graph, class Metric>
+    /// @param cmp The weight comparator functor,
+	template <class Graph, class Metric, typename WeightCmp>
 	void bellman_ford_relax(
-			const Graph& g, const Metric& m,
+			const Graph& g,
+            const Metric& m,
 			node src,
 			std::vector<node>& out_preds,
-			std::vector<typename Metric::weight_type>& out_dists) {
+			std::vector<typename Metric::weight_type>& out_dists,
+            const WeightCmp& cmp) {
 
 		const node N = static_cast<node>(nodes_count(g));
 
@@ -168,9 +162,9 @@ namespace detail {
 		out_dists[src] = weight_limits<typename Metric::weight_type>::zero();
 
 		for (node i = 0; i < (N - 1); ++i) {
-			for_each_edge(g, [&out_dists, &out_preds, &m](node u, node v) {
+			for_each_edge(g, [&out_dists, &out_preds, &m, &cmp] mutable (node u, node v) {
 				typename Metric::weight_type new_dist = out_dists[u] + m(edge(u, v));
-				if (out_dists[v] > new_dist) {
+				if (cmp(new_dist, out_dists[v])) {
 					out_dists[v] = new_dist;
 					out_preds[v] = u;
 				}
@@ -183,27 +177,27 @@ namespace detail {
 // The convenient API for the topological optimization algorithms.
 // ===============================================================
 
-template <class Graph, class Metric>
-path dijkstra(const Graph& g, const Metric& m, node src, node dst) {
+template <class Graph, class Metric, typename WeightCmp = std::less<typename Metric::weight_type>>
+path dijkstra(const Graph& g, const Metric& m, node src, node dst, const WeightCmp& cmp = WeightCmp {}) {
 	std::vector<node> preds;
 	std::vector<typename Metric::weight_type> dists;
-	detail::dijkstra_relax(g, m, detail::dst_stop{ dst }, src, preds, dists);
+	detail::dijkstra_relax(g, m, src, preds, dists, detail::dst_stop{ dst }, cmp);
 	return build_path(src, dst, preds);
 }
 
-template <class Graph, class Metric>
-tree prim(const Graph& g, const Metric& m, node src) {
+template <class Graph, class Metric, typename WeightCmp = std::less<typename Metric::weight_type>>
+tree prim(const Graph& g, const Metric& m, node src, const WeightCmp& cmp = WeightCmp {}) {
 	std::vector<node> preds;
 	std::vector<typename Metric::weight_type> dists;
-	detail::dijkstra_relax(g, m, detail::never_stop{}, src, preds, dists);
+	detail::dijkstra_relax(g, m, src, preds, dists, detail::never_stop{}, cmp);
 	return build_tree(preds);
 }
 
-template <class Graph, class Metric>
-path bellman_ford(const Graph& g, const Metric& m, node src, node dst) {
+template <class Graph, class Metric, typename WeightCmp = std::less<typename Metric::weight_type>>
+path bellman_ford(const Graph& g, const Metric& m, node src, node dst, const WeightCmp& cmp = WeightCmp {}) {
 	std::vector<node> preds;
 	std::vector<typename Metric::weight_type> dists;
-	detail::bellman_ford_relax(g, m, src, preds, dists);
+	detail::bellman_ford_relax(g, m, src, preds, dists, cmp);
 	return build_path(src, dst, preds);
 }
 
